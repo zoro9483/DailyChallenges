466d15c6ccc4677f53c3937a4dc44c3f9acafb12
zoro9483 <osama.hamed94@eng-st.cu.edu.eg>
2020-11-13 14:00:31 +0200

    adding js code to hide element and unhide it

---
+++ logic.js
@@ -0,0 +1,10 @@
+
+
+function hide(id){
+    var e = document.getElementById(id);
+
+    if(e.style.display== 'block')
+        e.style.display = 'none';
+        else
+        e.style.display = 'block';
+}
