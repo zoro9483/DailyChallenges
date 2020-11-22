b934b0ed03180f350f09873d6d0f2474ccdca0e3
zoro9483 <osama.hamed94@eng-st.cu.edu.eg>
2020-11-22 10:39:47 +0200

    adding a new style to show data/content on the page

--- logic.js
+++ logic.js
@@ -47,3 +47,18 @@ function openPage(pageName,elmnt,color) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
