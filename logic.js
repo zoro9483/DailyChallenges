ed0c86a7add73c4b296f4cd262051099f57cebe5
zoro9483 <osama.hamed94@eng-st.cu.edu.eg>
2020-11-27 14:48:42 +0200

    deleting not working part

--- logic.js
+++ logic.js
@@ -6,26 +6,26 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+//DIDN'T WORK AT ALL 
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// for (i = 0; i < coll.length; i++) {
 
-for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
 
-  coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
 
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
+//     if (content.style.maxHeight){
 
-    if (content.style.maxHeight){
-
-      content.style.maxHeight = null;
-    } else {
+//       content.style.maxHeight = null;
+//     } else {
       
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
 
 // function hide(id){
 //     var e = document.getElementById(id);
